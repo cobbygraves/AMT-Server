8b174f136044096956b2d446b9b3ac7d0ca33fd0
cobbygraves <rmeocnis@hotmail.com>
2025-06-25 18:55:26 +0000

    feat:archive route

--- controllers/notes.js
+++ controllers/notes.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require('uuid')
 const moment = require('moment')
 
 const getNotes = async (req, res) => {
-  const notes = await NoteModel.find({ isArchived: false }).sort({
+  const notes = await NoteModel.find({}).sort({
     createdAt: -1
   })
   return res.json(notes)
@@ -21,7 +21,39 @@ const createNote = async (req, res) => {
   }
 }
 
+const archiveNote = async (req, res) => {
+  const noteId = req.params.id
+  const { isArchived } = req.query
+
+  try {
+    const resp = await NoteModel.updateOne({ id: noteId }, { isArchived })
+    return res.json(resp)
+  } catch (error) {
+    res.status(400).json(error)
+  }
+}
+
+const getSingleNote = async (req, res) => {
+  const noteId = req.params.id
+  const note = await NoteModel.findOne({ id: noteId })
+  return res.json(note)
+}
+
+const updateNote = async (req, res) => {
+  const note = req.body
+  const id = req.params.id
+  try {
+    const resp = await NoteModel.updateOne({ id: id }, note)
+    return res.json(resp)
+  } catch (error) {
+    res.status(400).json(error)
+  }
+}
+
 module.exports = {
   getNotes,
-  createNote
+  createNote,
+  archiveNote,
+  getSingleNote,
+  updateNote
 }
--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,17 @@
 const express = require('express')
-const { getNotes, createNote } = require('../controllers/notes')
+const {
+  getNotes,
+  createNote,
+  archiveNote,
+  getSingleNote,
+  updateNote
+} = require('../controllers/notes')
 
 const router = express.Router()
 router.get('/', getNotes)
 router.post('/create', createNote)
+router.get('/:id/archive', archiveNote)
+router.get('/:id', getSingleNote)
+router.put('/:id', updateNote)
 
 module.exports = router
